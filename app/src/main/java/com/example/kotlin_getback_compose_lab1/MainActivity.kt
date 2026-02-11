fa40c2b87257b791b5e7bb28b6ae6bfe9baaf76e
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-02-11 20:57:03 +0200

    add ui jetback compose

--- app/src/main/java/com/example/kotlin_getback_compose_lab1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_getback_compose_lab1/MainActivity.kt
@@ -1,128 +1,193 @@
 package com.example.kotlin_getback_compose_lab1
 
-import android.R.attr.onClick
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.kotlin_getback_compose_lab1.data_class.ListItem
+import android.content.res.Configuration
+import androidx.activity.compose.BackHandler
 import com.example.kotlin_getback_compose_lab1.data_class.Product
-
-
-
 import androidx.compose.runtime.*
-
-import androidx.work.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    private val productsState = mutableStateOf<List<Product>>(emptyList())
+    private lateinit var dao: ProductDao
+    private var products by mutableStateOf<List<Product>>(emptyList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
-        startWorker()
-        observeWorker()
+        dao = ProductDatabase.getDB(this).productDao()
+
+        lifecycleScope.launch {
+            loadProducts()
+        }
 
         setContent {
-            MyLazyList(productsState.value)
+            MainScreen(products)
         }
     }
 
-    private fun startWorker() {
-        val request = OneTimeWorkRequestBuilder<ProductWorker>()
-            .addTag("ProductWorker")
-            .build()
+    private suspend fun loadProducts() {
 
-        WorkManager.getInstance(this).enqueue(request)
-    }
-
-    private fun observeWorker() {
-        WorkManager.getInstance(this)
-            .getWorkInfosByTagLiveData("ProductWorker")
-            .observe(this) { workInfos ->
-                val data = workInfos.firstOrNull()?.outputData?.getString("products")
-                data?.let {
-                    val type = object : TypeToken<List<Product>>() {}.type
-                    val products: List<Product> = Gson().fromJson(it, type)
-                    productsState.value = products
-                }
-            }
-    }
-
-    @Composable
-    fun MyLazyList(products: List<Product>) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            items(products) { product ->
-                ListRow(product)
-            }
+        if (NetworkUtil.isConnected(this)) {
+            val response = RetrofitInstance.api.getProducts().products
+            dao.insertAll(response)
+            products = response
+        } else {
+            products = dao.getAll()
         }
     }
+}
+@Composable
+fun MainScreen(products: List<Product>) {
 
-    @OptIn(ExperimentalGlideComposeApi::class)
-    @Composable
-    fun ListRow(product: Product) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(22.dp).clickable(
-                    onClick = {
-                        Toast.makeText(this, product.title, Toast.LENGTH_SHORT).show()
-                    }
-                ),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+    var selectedProductId by rememberSaveable { mutableStateOf<Int?>(null) }
 
-            GlideImage(
-                model = product.thumbnail,
-                contentDescription = product.title,
-                modifier = Modifier
-                    .size(60.dp)
-                    .clip(RoundedCornerShape(8.dp))
+    val selectedProduct = products.find { it.id == selectedProductId }
+
+    val isLandscape =
+        LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+    BackHandler(enabled = selectedProduct != null && !isLandscape) {
+        selectedProductId = null
+    }
+
+    if (isLandscape) {
+
+        Row(Modifier.fillMaxSize()) {
+
+            ProductList(
+                products = products,
+                onClick = { selectedProductId = it.id },
+                modifier = Modifier.weight(1f)
             )
 
-            Spacer(modifier = Modifier.width(12.dp))
-
-            Column {
-                Text(text = product.title)
-                Text(
-                    text = product.description,
-                    color = Color.Gray
+            selectedProduct?.let {
+                ProductDetails(
+                    product = it,
+                    modifier = Modifier.weight(1f)
                 )
+            }
+        }
+
+    } else {
+
+        if (selectedProduct == null) {
+
+            ProductList(
+                products = products,
+                onClick = { selectedProductId = it.id }
+            )
+
+        } else {
+
+            ProductDetails(
+                product = selectedProduct,
+              //  onBack = { selectedProductId = null }
+            )
+        }
+    }
+}
+
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun ProductList(
+    products: List<Product>,
+    onClick: (Product) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    LazyColumn(modifier) {
+
+        items(products) { product ->
+
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable { onClick(product) }
+            ) {
+
+                GlideImage(
+                    model = product.thumbnail,
+                    contentDescription = null,
+                    modifier = Modifier.size(60.dp)
+                )
+
+                Spacer(Modifier.width(12.dp))
+
+                Column {
+                    Text(product.title)
+                    Spacer(Modifier.height(6.dp))
+                    Text(product.description)
+                }
             }
         }
     }
 }
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun ProductDetails(
+    product: Product,
+    modifier: Modifier = Modifier,
+//    onBack: (() -> Unit)? = null
+) {
+
+    Column(modifier.padding(36.dp)) {
+
+//        onBack?.let {
+//            Text(
+//                "Back",
+//                modifier = Modifier
+//                    .clickable { it() }
+//                    .padding(bottom = 8.dp),
+//                color = Color.Blue
+//            )
+//        }
+
+        GlideImage(
+            model = product.thumbnail,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp)
+        )
+
+        Spacer(Modifier.height(12.dp))
+
+        Text(text = product.title)
+
+        Spacer(Modifier.height(8.dp))
+
+        Text(text = product.description)
+    }
+}
+
+
 
 
 
