c9f0a3c3c90af53d126818a0f1e55d66c95faff6
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-02-11 20:56:35 +0200

    add product data class

--- app/src/main/java/com/example/kotlin_getback_compose_lab1/data_class/Product.kt
+++ app/src/main/java/com/example/kotlin_getback_compose_lab1/data_class/Product.kt
@@ -1,11 +1,22 @@
 package com.example.kotlin_getback_compose_lab1.data_class
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//data class Product(
+//    val id: Int,
+//    val title: String,
+//    val description: String,
+//    val thumbnail: String
+//)
+@Entity(tableName = "products")
 data class Product(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val title: String,
     val description: String,
     val thumbnail: String
 )
+
 data class ProductResponse(
     val products: List<Product>
 )
