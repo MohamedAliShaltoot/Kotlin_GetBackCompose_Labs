e5b2cd1163bb5000e5acb65aa77f92af87095308
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-02-13 00:35:19 +0200

    removes old files

--- app/src/main/java/com/example/kotlin_getback_compose_lab1/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlin_getback_compose_lab1
-
-import com.example.kotlin_getback_compose_lab1.data_class.ProductResponse
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("products")
-    suspend fun getProducts(): ProductResponse
-}
